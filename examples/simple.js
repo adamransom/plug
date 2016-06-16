a5b7662f916b7359b0d3fe516f1d0053fec0edc0
Adam Ransom <v0id.gmr@gmail.com>
2016-06-16 20:54:07 +0900

    add extremely basic library setup

---
+++ examples/simple.js
@@ -0,0 +1,8 @@
+// When using as a real npm module, replace the below with:
+//
+//   const Plug = require('plug-ai');
+//
+const Plug = require('../');
+
+// Create a new bot instance
+const bot = new Plug();
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import Plug from './lib/plug';
+
+// Use CommonJS exports so this library can easily be used
+// in ES5 environments as well.
+module.exports = Plug;
